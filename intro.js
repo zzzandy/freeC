0663030c0a786b5ae3341112fa50e2e9171062c4
zzzAndy <fruit.kefir@yandex.ru>
2023-10-20 23:45:31 +0300

    20%

---
+++ intro.js
@@ -0,0 +1,82 @@
+// #8
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver  = 9000;
+
+// #9
+let  catName = "Oliver";
+let  catSound = "Meow!";
+
+// #10
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// #11
+const sum = 10 + 10;
+
+// #12
+const difference = 45 - 33;
+
+// #13
+const product = 8 * 10;
+
+// #14
+const quotient = 66 / 33;
+
+// #15
+let myVar1 = 87;
+
+// Only change code below this line
+myVar = ++myVar;
+
+// #16
+let myVar2 = 11;
+
+// Only change code below this line
+myVar = --myVar;
+
+// #17
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+let myDecimal  = 5.7;
+
+// #18
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// #19
+let a1 = 11;
+let b1 = 9;
+let c1 = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -=  1;
+
+// #20
+
+let a2 = 5;
+let b2 = 12;
+let c2 = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
