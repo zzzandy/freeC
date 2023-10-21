19cf566f86293463f937b4f09c983ae7a623f177
zzzAndy <fruit.kefir@yandex.ru>
2023-10-21 16:45:44 +0300

    50%

--- intro.js
+++ intro.js
@@ -7,11 +7,11 @@ var titleCaseOver;
 // Variable assignments
 studlyCapVar = 10;
 properCamelCase = "A String";
-titleCaseOver  = 9000;
+titleCaseOver = 9000;
 
 // #9
-let  catName = "Oliver";
-let  catSound = "Meow!";
+let catName = "Oliver";
+let catSound = "Meow!";
 
 // #10
 const FCC = "freeCodeCamp"; // Change this line
@@ -48,7 +48,7 @@ const ourDecimal = 5.7;
 
 // Only change code below this line
 
-let myDecimal  = 5.7;
+let myDecimal = 5.7;
 
 // #18
 let a = 3;
@@ -68,10 +68,9 @@ let c1 = 3;
 // Only change code below this line
 a -= 6;
 b -= 15;
-c -=  1;
+c -= 1;
 
 // #20
-
 let a2 = 5;
 let b2 = 12;
 let c2 = 4.6;
@@ -80,3 +79,104 @@ let c2 = 4.6;
 a *= 5;
 b *= 3;
 c *= 10;
+
+// #21
+let a3 = 48;
+let b3 = 108;
+let c3 = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+// #22
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+// #23
+const myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// #24
+const myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+// #25
+const myStr3 = "This is the start. " + "This is the end."; // Change this line
+
+// #26
+let myStr4 = 'This is the first sentence. ';
+myStr4 += 'This is the second sentence.';
+
+// #27
+// Only change code below this line
+const myName = "zzzandy";
+const myStr5 = "My name is " + myName + ' and I am well!';
+
+// #28
+// Change code below this line
+const someAdjective = "zzz";
+let myStr6 = "Learning to code is ";
+myStr += someAdjective;
+
+
+// #29
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// #30
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// #31
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// #32
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// #33
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+// #34
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+// #35
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun + ' ' + myVerb + ' ' + myAdjective + ' ' + myAdverb; // Change this line
+// Only change code above this line
+
+// #36
+
+
+// #30
+// #30
