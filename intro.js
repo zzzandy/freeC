69d0ac0fb83a4059be48921f307772f32cec0950
zzzAndy <fruit.kefir@yandex.ru>
2023-10-23 18:03:26 +0300

    100%

--- intro.js
+++ intro.js
@@ -556,8 +556,137 @@ function updateRecords(records, id, prop, value) {
 //updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 updateRecords(recordCollection, 5439, 'artist', '');
 
-// #50
-// #50
+// #57
+function sum(arr, n) {
+  // Only change code below this line
+    if (n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+  // Only change code above this line
+}
+
+// #58
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+
+  // Only change code below this line
+  for(let i = 0; i < contacts.length; i++){
+
+      if(contacts[i].firstName === name){
+
+      if(!contacts[i].hasOwnProperty(prop)){
+        return 'No such property';
+      }
+      return contacts[i][prop]
+      }      
+  }
+  return 'No such contact';
+  // Only change code above this line
+}
+
+//lookUpProfile("Kristian", "lastName");
+lookUpProfile("Bob", "number");
+lookUpProfile("Akira", "address");
+
+// #59
+
+function convertToInteger(str) {
+  return parseInt(str)
+  }
+  
+  convertToInteger("56");
+
+// #60
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+
+// #61
+
+function checkEqual(a, b) {
+  return a === b ? 'Equal' : 'Not Equal'
+  }
+  
+  checkEqual(1, 2);
+
+// #62
+function checkSign(num) {
+return (num === 0) ? 'zero':
+(num > 0) ? 'positive' :
+'negative' 
+}
+
+checkSign(10);
+
+
+// #63
+
+// Only change code below this line
+function countdown(n){
+  if(n < 1) { return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n)
+    return arr;
+  }
+}
+console.log(countdown(5));
+// Only change code above this line
+
+// #64
+
+function rangeOfNumbers(startNum, endNum) {
+  if(startNum > endNum) return [];
+  else {
+    const arr = rangeOfNumbers(startNum, endNum - 1);
+    console.log(arr);
+    arr.push(endNum)
+    //console.log(arr);
+  
+    return arr;
+  }
+};
+  
+rangeOfNumbers(1,5)
+// #60
+// #60
+// #60
+// #60
+// #60
+// #60
+// #60
 
 
 
