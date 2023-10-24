c9f4b580ff51a70b4573cfed9a575510b8a38669
zzzAndy <fruit.kefir@yandex.ru>
2023-10-24 22:15:46 +0300

    80%

--- es6.js
+++ es6.js
@@ -173,4 +173,99 @@ function removeFirstTwo(list) {
 
   // #16
 
+  const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {name, age, gender };
+    // Only change code above this line
+  };
+
+  // #17
+
+  // Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+      this.gear = newGear;
+    }
+  };
+  // Only change code above this line
+  bicycle.setGear(3);
+  console.log(bicycle.gear);
+
+  // #18
+
+  // Only change code below this line
+class Vegetable {
+    constructor(val) {
+      this.name = val;
+    }
+  }
+  // Only change code above this line
   
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // Should display 'carrot'
+
+  // #19
+
+  // Only change code below this line
+class Thermostat {
+    constructor(tempF){
+      this._tempF = tempF;
+    }
+    get temperature(){
+      return (5/9 * ( this._tempF - 32));
+    }
+    set temperature(updateTempF){
+      this._tempF = (updateTempF * 9.0 / 5 + 32)
+    }
+  
+  }
+  // Only change code above this line
+  const thermos2 = new Thermostat(68);
+  console.log(thermos2)
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
+
+  // # 20
+
+  <html>
+  <body>
+    <!-- Only change code below this line -->
+  <script type="module" src="index.js"></script>
+    <!-- Only change code above this line -->
+  </body>
+</html>
+
+// #21
+
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+  }
+  
+  const lowercaseString = (string) => {
+    return string.toLowerCase()
+  }
+  
+  export {uppercaseString, lowercaseString}
+
+  // #22
+
+  import {uppercaseString, lowercaseString} from './string_functions.js'
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+// #23
+
+import * as stringFunctions from './string_functions.js' 
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
+
+// #24
+
