bfee8766cdc764f97b9b239bd9ea6cff36c2d2cd
zzzAndy <fruit.kefir@yandex.ru>
2024-10-03 22:10:05 +0300

    codewars

---
+++ codewars.js
@@ -0,0 +1,18 @@
+const rps = (p1, p2) => {
+    const rules = {
+    rock: 'scissors',
+    scissors: 'paper',
+    paper: 'rock'
+  }
+    
+    if(p1 === p2) return "Draw!"
+    
+    if(rules[p1] === p2){
+    return 'Player 1 won!'
+  } else {
+    return 'Player 2 won!' 
+  }
+  };
+  
+  
+  
