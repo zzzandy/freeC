e86b2e362413a805be1a5a531a2652c4af05b2a8
zzzAndy <fruit.kefir@yandex.ru>
2024-10-04 11:21:35 +0300

    beautyf

--- codewars.js
+++ codewars.js
@@ -1,28 +1,28 @@
 const rps = (p1, p2) => {
-    const rules = {
-    rock: 'scissors',
-    scissors: 'paper',
-    paper: 'rock'
-  }
-    
-    if(p1 === p2) return "Draw!"
-    
-    if(rules[p1] === p2){
-    return 'Player 1 won!'
-  } else {
-    return 'Player 2 won!' 
-  }
-  };
-  
-  
-  function circleArea(circle){
-    return Math.PI * circle.radius ** 2
-  }
+const rules = {
+rock: 'scissors',
+scissors: 'paper',
+paper: 'rock'
+}
 
-  const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-    return (distanceToPump <= mpg * fuelLeft) ? true : false
-    }
+if(p1 === p2) return "Draw!"
 
-    function toBinary(n){
-        return +n.toString(2);
-      }
+if(rules[p1] === p2){
+return 'Player 1 won!'
+} else {
+return 'Player 2 won!' 
+}
+};
+
+
+function circleArea(circle){
+return Math.PI * circle.radius ** 2
+}
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+return (distanceToPump <= mpg * fuelLeft) ? true : false
+}
+
+function toBinary(n){
+    return +n.toString(2);
+}
